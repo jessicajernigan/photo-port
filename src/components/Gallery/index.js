3f4e6b4fc0cd251ca1488550227220099fe1e768
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-29 17:35:53 -0500

    fixed Gallery index file issue

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import PhotoList from '../PhotoList';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import { capitalizeFirstLetter } from '../../utils/helpers'
+import PhotoList from '../PhotoList'
 
 function Gallery({ currentCategory }) {
-  const { name, description } = currentCategory;
-  return (
-    <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-      <p>{description}</p>
-      <PhotoList  />
-    </section>
-  );
+  const { name, description } = currentCategory
+	return (
+		<section>
+			<h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+			<p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name}/>
+		</section>
+	);
 }
 
 export default Gallery;
