c57e3132ed3fd5c5ca3973b8715bb9ea8731f23f
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-29 13:50:09 -0500

    fixed stylesheet issues

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,9 +3,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-
 afterEach(cleanup);
-
 
 describe('Nav component', () => {
   // baseline test
@@ -16,18 +14,18 @@ describe('Nav component', () => {
   // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-  
+
     expect(asFragment()).toMatchSnapshot();
-  });  
+  });
 })
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-  const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav />);
 
-  expect(getByLabelText('camera')).toHaveTextContent('📸');
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
